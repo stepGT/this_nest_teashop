adced45ee0ed32849221c59de60d6e11339af8d3
Andrey Zuev <stepgt@mail.ru>
2025-10-30 16:29:56 +1000

    Added JWT strategy

---
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { UserService } from '../../user/user.service'
+
+@Injectable()
+export class JWTStrategy extends PassportStrategy(Strategy) {
+	constructor(
+		private configService: ConfigService,
+		private userService: UserService
+	) {
+		// @ts-ignore
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: true,
+			secretOrKey: configService.get('JWT_SECRET')
+		})
+	}
+
+	async validate({ id }: { id: string }) {
+		return this.userService.getByID(id)
+	}
+}
