30be434ce20b4e5966b933d1e5494f8ebd21e8c5
Andrey Zuev <stepgt@mail.ru>
2025-10-30 11:40:08 +1000

    Added user service getByID, getByEmail & create

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { AuthDTO } from '../auth/dto/auth.dto'
+import { hash } from 'argon2'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(private readonly prisma: PrismaService) {}
+
+	async getByID(id: string) {
+		const user = await this.prisma.user.findUnique({
+			where: { id },
+			include: {
+				stores: true,
+				favorites: true,
+				orders: true
+			}
+		})
+		return user
+	}
+
+	async getByEmail(email: string) {
+		const user = await this.prisma.user.findUnique({
+			where: { email },
+			include: {
+				stores: true,
+				favorites: true,
+				orders: true
+			}
+		})
+		return user
+	}
+
+	async create(dto: AuthDTO) {
+		return this.prisma.user.create({
+			data: {
+				name: dto.name,
+				email: dto.email,
+				password: await hash(dto.password)
+			}
+		})
+	}
+}
