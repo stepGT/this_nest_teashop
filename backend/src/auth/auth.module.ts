894beafc97f7f4ad5d3113bb7af3bb0aab43cdba
Andrey Zuev <stepgt@mail.ru>
2025-11-01 14:17:55 +1000

    Auth module edited imports & providers

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { getJwtConfig } from 'src/config/jwt.config';
+import { PrismaService } from 'src/prisma.service';
+import { UserService } from 'src/user/user.service';
+import { JWTStrategy } from './strategies/jwt.strategy';
+import { GoogleStrategy } from './strategies/google.strategy';
+import { YandexStrategy } from './strategies/yandex.strategy';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+	imports: [
+		UserModule,
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		})
+	],
+	controllers: [AuthController],
+	providers: [
+		AuthService,
+		PrismaService,
+		UserService,
+		JWTStrategy,
+		GoogleStrategy,
+		YandexStrategy
+	]
 })
 export class AuthModule {}
