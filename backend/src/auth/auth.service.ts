f1ed17f5772366d998edc26843f06aaae8998c40
Andrey Zuev <stepgt@mail.ru>
2025-11-01 14:36:01 +1000

    Added auth services login, register, issueTokens & validateUser

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
+import { UserService } from 'src/user/user.service';
+import { AuthDTO } from './dto/auth.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(
+		private jwt: JwtService,
+		private userService: UserService,
+		private prisma: PrismaService
+	) {}
+
+	async login(dto: AuthDTO) {
+		const user = await this.validateUser(dto);
+		const tokens = this.issueTokens(user.id);
+		return { user, ...tokens };
+	}
+
+	async register(dto: AuthDTO) {
+		const oldUser = await this.userService.getByEmail(dto.email);
+		if (oldUser) throw new BadRequestException('User not found!');
+		const user = await this.userService.create(dto);
+		const tokens = this.issueTokens(user.id);
+		return { user, ...tokens };
+	}
+
+	issueTokens(userID: string) {
+		const data = { id: userID };
+
+		const accessToken = this.jwt.sign(data, {
+			expiresIn: '1h'
+		});
+
+		const refreshToken = this.jwt.sign(data, {
+			expiresIn: '1h'
+		});
+		return { accessToken, refreshToken };
+	}
+
+	private async validateUser(dto: AuthDTO) {
+		const user = await this.userService.getByEmail(dto.email);
+		if (!user) {
+			throw new NotFoundException('User not found!');
+		}
+		return user;
+	}
+}
