dfd326ec9c63b4352990d45a6e9bce867ff740dc
Andrey Zuev <stepgt@mail.ru>
2025-11-14 11:02:40 +1000

    Added entities color, category, file, store, order, statistics & product

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ColorModule } from './color/color.module';
+import { CategoryModule } from './category/category.module';
+import { FileModule } from './file/file.module';
+import { StoreModule } from './store/store.module';
+import { OrderModule } from './order/order.module';
+import { StatisticsModule } from './statistics/statistics.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule]
+	imports: [ConfigModule.forRoot(), AuthModule, UserModule, ColorModule, CategoryModule, FileModule, StoreModule, OrderModule, StatisticsModule, ProductModule]
 })
 export class AppModule {}
