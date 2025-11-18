06f254894ce7a358a5d6eb02fc27e187a2605683
Andrey Zuev <stepgt@mail.ru>
2025-11-18 16:16:19 +1000

    Added store service getByID & Added store providers PrismaService

--- backend/src/store/store.service.ts
+++ backend/src/store/store.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class StoreService {}
+export class StoreService {
+	constructor(private prisma: PrismaService) {}
+
+	async getByID(storeID: string, userID: string) {
+		const store = await this.prisma.store.findUnique({
+			where: { id: storeID, userID }
+		});
+		if (!store)
+			throw new NotFoundException(
+				'The store was not found or you are not the owner!'
+			);
+		return store;
+	}
+}
