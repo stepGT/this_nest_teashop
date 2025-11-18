e106afc41c4b9fe643bf8627b4bf541a6a7ebb22
Andrey Zuev <stepgt@mail.ru>
2025-11-18 16:07:48 +1000

    Added user controllers getProfile & toggleFavorites

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Patch } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from './decorators/user.decorator';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
+
+	@Auth()
+	@Get('profile')
+	async getProfile(@CurrentUser('id') id: string) {
+		return this.userService.getByID(id);
+	}
+
+	@Auth()
+	@Patch('profile/favorites/:productID')
+	async toggleFavorites(
+		@CurrentUser('id') userID: string,
+		@Param('productID') productID: string
+	) {
+		return this.userService.togglleFavorite(productID, userID);
+	}
 }
